4bf5e97bbd7b07d7adb03f062b1ba62280e88a15
jsorribes <lo_sorry@hotmail.com>
2017-05-26 12:06:16 +0200

    prueba de bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'statics/index'
+
+  get 'statics/about'
+  get '/about' => 'static#about', :as => :about
+	root :to => "statics#index"
   devise_for :users
   get 'peliculas/index'
-	root :to => "peliculas#index"
+	# root :to => "peliculas#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
